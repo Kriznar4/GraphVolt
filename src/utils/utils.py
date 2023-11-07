45ba3f30dad53b6b2e071fb67bf5a956acd2c884
Kriznar4 <karel.kriznar98@gmail.com>
2023-11-07 07:37:48 +0100

    updated read_network_data for new data tables

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-def read_network_data(trafo_id, depth=1):
+def read_raw_network_data(trafo_id, depth=1):
     """
     Reads all csv files from a given transformer stations. Depth is number of parent filders to get to GraphVold folder.
     """
@@ -12,10 +12,10 @@ def read_network_data(trafo_id, depth=1):
     #get data folder and then to networks_raw_folder
     path_data_raw = os.path.join(parent_dir, 'data', 'networks_data_raw')
 
-    tablenames = ["bus", "bus_geodata", "EnergijaSMM", "ext_grid", "line", "line_geodata", "load", "NapetostiSMM", "tehnicni", "TPnapetosti_energija", "trafo"]
+    tablenames = ["lines_static_data", "nodes_static_data", "SMM_measurements", "TP_measurements"]
 
     #get path to network
-    path_network = os.path.join(path_data_raw, f"{trafo_id}_anon")
+    path_network = os.path.join(path_data_raw, f"{trafo_id}_anon_procesed")
 
     #read all csv files from path_network
     df_network_dict = {}
@@ -23,4 +23,4 @@ def read_network_data(trafo_id, depth=1):
         path_table = os.path.join(path_network, f"{trafo_id}_{tablename}.csv")
         df_network_dict[tablename] = pd.read_csv(path_table, sep=",", decimal=".")
     
-    return df_network_dict
+    return df_network_dict, path_network
